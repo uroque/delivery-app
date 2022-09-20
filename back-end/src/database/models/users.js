ecfebac6e2157b73a6434c951a9e65b444340365
fabionicolau <fabionicolaujr@gmail.com>
2022-09-20 16:45:01 -0300

    feat: create migrations, models and seeders for users

---
+++ back-end/src/database/models/users.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const User = (sequelize, DataTypes) => {
+  const users = sequelize.define('users', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+  }, {
+    tablename: 'users',
+    timestamps: false,
+    underscored: false,
+  });
+
+  users.associate = (models) => {
+    users.hasMany(models.sales, { foreignKey: 'userId', as: 'sales' });
+  };
+
+  return users;
+}
+
