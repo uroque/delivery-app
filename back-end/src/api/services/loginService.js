76b74c1af1b1f578c7d35a148c6b6451f293f178
fabionicolau <fabionicolaujr@gmail.com>
2022-09-21 19:32:13 -0300

    feat: add password validation with md5 hash generator

--- back-end/src/api/services/loginService.js
+++ back-end/src/api/services/loginService.js
@@ -1,10 +1,14 @@
 const { users } = require('../../database/models');
+const md5 = require('md5');
 
-const postLogin = async (email, _password) => {
+const postLogin = async (email, password) => {
     const database = await users.findOne({ where: { email } });
     if (!database) return null;
     const emailData = database.email;
-    if (email !== emailData) return null;    
+    const passwordData = database.password
+    if (email !== emailData) return null;
+    if (md5(password) !== passwordData) return null
+
     return {
       name: database.name,
       email: database.email,
