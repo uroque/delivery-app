894f03c6267cdf88b7df4a8fab9f1fcc26aab05a
Ulisses Roque <ulisses.roque@gmail.com>
2022-09-22 17:54:38 -0300

    test: fixing general problems

--- front-end/src/tests/unit/ClientPages/orderDetails.test.js
+++ front-end/src/tests/unit/ClientPages/orderDetails.test.js
@@ -4,15 +4,13 @@ import { screen } from '@testing-library/react';
 import renderWithRouter from '../../utils/renderWithRouter';
 import App from '../../../App';
 
-const customerProductsPath = '/costumer/products';
+const customerOrdersDetailsPath = '/costumer/orders/1';
 
-jest.mock();
-
-describe('Customer Products page', () => {
+describe('Customer Order Details page', () => {
   describe('Navigation bar', () => {
     it(`Have a button with 
     data-test-id="customer_products__element-navbar-link-products"`, () => {
-      renderWithRouter(<App />, { route: customerProductsPath });
+      renderWithRouter(<App />, { route: customerOrdersDetailsPath });
 
       const productsButton = screen.getByTestId((
         'customer_products__element-navbar-link-products'));
@@ -22,7 +20,7 @@ describe('Customer Products page', () => {
 
     it(`Have password input with 
     data-test-id="customer_products__element-navbar-link-orders"`, () => {
-      renderWithRouter(<App />, { route: customerProductsPath });
+      renderWithRouter(<App />, { route: customerOrdersDetailsPath });
 
       const ordersButton = screen
         .getByTestId(('customer_products__element-navbar-link-orders'));
@@ -32,7 +30,7 @@ describe('Customer Products page', () => {
 
     it(`Have an element with 
     data-test-id="customer_products__element-navbar-user-full-name"`, () => {
-      renderWithRouter(<App />, { route: customerProductsPath });
+      renderWithRouter(<App />, { route: customerOrdersDetailsPath });
 
       const passwordInput = screen.getByTestId((
         'customer_products__element-navbar-user-full-name'));
@@ -42,7 +40,7 @@ describe('Customer Products page', () => {
 
     it(`Have a button with 
     data-test-id="customer_products__element-navbar-link-logout"`, () => {
-      renderWithRouter(<App />, { route: customerProductsPath });
+      renderWithRouter(<App />, { route: customerOrdersDetailsPath });
 
       const logoutButton = screen
         .getByTestId(('customer_products__element-navbar-link-logout'));
@@ -52,17 +50,17 @@ describe('Customer Products page', () => {
 
     // it(`Click on customer_products__element-navbar-link-logout redirects the user to
     // /login`, () => {
-    //   renderWithRouter(<App />, { route: customerProductsPath });
+    //   renderWithRouter(<App />, { route: customerOrdersDetailsPath });
 
     //   const logoutButton = screen.getByTestId('common_register__button-register');
-    //   fireEvent.click(logoutButton);
+    //   userEvent.click(logoutButton);
 
     //   expect(history.location.pathname).toBe('/login');
     // });
 
     it(`Have an element with 
     data-test-id="customer_products__checkout-bottom-value"`, () => {
-      renderWithRouter(<App />, { route: customerProductsPath });
+      renderWithRouter(<App />, { route: customerOrdersDetailsPath });
 
       const passwordInput = screen.getByTestId((
         'customer_products__checkout-bottom-value'));
