3dbcc65b37a289e5906ef629681293a57a8a4134
henrique <henrique.campana22@gmail.com>
2022-09-22 16:40:38 -0300

    feat: Create Card Component

---
+++ front-end/src/components/Card.jsx
@@ -0,0 +1,56 @@
+import React, { useContext, useEffect } from 'react';
+import ProductsContext from '../context/productsContext/context';
+
+function Card() {
+  const { productList, setProducts } = useContext(ProductsContext);
+
+  useEffect(() => {
+    async function getAllProducts() {
+      const request = await fetch('http://localhost:3003/customer/products', {
+        method: 'GET',
+      });
+      const response = await request.json();
+      setProducts(response);
+    }
+    getAllProducts();
+  }, [productList, setProducts]);
+
+  return (
+    <>
+      { productList.map((item) => (
+        <div key={ item.id }>
+          <h3 data-testid={ `customer_products__element-card-price-${item.id}` }>
+            {item.price}
+          </h3>
+          <img
+            src={ item.url_image }
+            alt={ item.name }
+            data-testid={ `customer_products__element-card-bg-image-${item.id}` }
+          />
+          <p data-testid={ `customer_products__element-card-title-${item.id}` }>
+            {item.name}
+          </p>
+          <button
+            type="button"
+            data-testid={ `customer_products__button-card-rm-item-${item.id}` }
+          >
+            -
+          </button>
+          <span
+            data-testid={ `customer_products__input-card-quantity-${item.id}` }
+          >
+            0
+          </span>
+          <button
+            type="button"
+            data-testid={ `customer_products__button-card-add-item-${item.id}` }
+          >
+            +
+          </button>
+        </div>
+      )) }
+    </>
+  );
+}
+
+export default Card;
